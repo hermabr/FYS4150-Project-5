a42cfec79ff77b63ab02e06586c2401196162b50
sim-hal <simon.halstensen@gmail.com>
2021-12-14 12:06:48 +0100

    docstrings

--- include/project5/crack_nicolson.hpp
+++ include/project5/crack_nicolson.hpp
@@ -14,16 +14,61 @@ class CrackSystem {
     private:
         // void initialize_A_B(int M, arma::cx_mat & A, arma::cx_mat & B, double dt, double h, arma::cx_mat v);
         int ij_to_k(int i, int j);
+        /**
+         * @brief Mapping indices j to values x
+         * 
+         * @param j 
+         * @return double 
+         */
         double j_to_x(int j);
+        /**
+         * @brief Mapping indices i to values y
+         * 
+         * @param i 
+         * @return double 
+         */
         double i_to_y(int i);
+        /**
+         * @brief Options for the SuperLU solver used in our Crank-Nicolson implementation
+         * 
+         */
         arma::superlu_opts opts;
         int M, M_star, M_star_square;
         double h, dt, T;
+        /**
+         * @brief The matricies required for the Crank-Nicolson approach
+         * 
+         */
         arma::sp_cx_mat A, B;
+        /**
+         * @brief The potential matrix
+         * 
+         */
         arma::sp_mat V;
+        /**
+         * @brief Holding the current state of the wave function
+         * 
+         */
         arma::cx_vec u;
+        /**
+         * @brief Setting up the matrices A and B required for the Crank-Nicolson approach
+         * 
+         */
         void initialize_A_B();
         arma::cx_vec solve_for_u_next(arma::cx_vec u);
+        /**
+         * @brief Initialize the vector u as according to a unnormalized gaussian wave packet, 
+         * before also normalizing so that the total probability (summing ecah element multiplied 
+         * with it's complex conjugate) is 1
+         * 
+         * @param x_c x-coordinate of the center of the initial wave packet
+         * @param y_c y-coordinate of the center of the initial wave packet
+         * @param sigma_x width in x-direction of the initial wave packet
+         * @param sigma_y width in y-direction of the initial wave packet
+         * @param p_x wave packet momenta in x-direction
+         * @param p_y wave packet momenta in y-direction
+         * @return arma::cx_vec u_0
+         */
         arma::cx_vec initialize_u(double x_c, double y_c, double sigma_x, double sigma_y, double p_x, double p_y);
         /**
          * @brief Initialize V for a double slit set up. Sets values at grid points where there is a wall to v_0, else 0
